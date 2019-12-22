ab06db4078cfa3cbb86e1921267b9c371fe7c1c2
Keyboard L <keyboard-l@outlook.com>
2019-12-22 20:09:45 +0800

    chore: 调整组件打包顺序

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+
+// Element UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+// FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faList, faTags, faUndo, faTrash, faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faList, faTags, faUndo, faTrash, faBars)
+import App from './App'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
+library.add(faList, faTags, faUndo, faTrash, faBars)
 Vue.component('fa-icon', FontAwesomeIcon)
 
 /* eslint-disable no-new */
